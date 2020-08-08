012d648655e4f2e43db7fdb2d2a596dc4d9a2568
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-08 18:39:46 +0530

    Create Kthsmallest prime.py

---
+++ Kthsmallest prime.py
@@ -0,0 +1,28 @@
+class Solution:
+  def kthSmallestPrimeFraction(self, A, K):
+    n = len(A)
+    l = 0.0
+    r = 1.0
+    while l < r:
+      m = (l + r) / 2
+      max_f = 0.0
+      total = 0      
+      j = 1
+      for i in range(n - 1):
+        while j < n and A[i] > m * A[j]: j += 1
+        if j == n: break
+        total += n - j
+        f = A[i] / A[j]
+        if f > max_f:
+          p, q, max_f = i, j, f
+      if total == K:
+        return [A[p], A[q]]
+      elif total > K:
+        r = m
+      else:
+        l = m
+    return []
+val=Solution()
+n,k=map(int,input().split())
+A=list(map(int,input().split()))
+print(val.kthSmallestPrimeFraction(A,k))
